af2f9649d4daa2c6f5f3ab1508fdb10179f5127b
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-26 00:13:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,7 @@ new_hash = {}
 
 def reformat_languages(languages)
   languages.each do |oo, functional|
-    functional.each do |language, 
+    functional.each do |language, parameters|
+      parameters.each do |type, values|
+        if language
 end
